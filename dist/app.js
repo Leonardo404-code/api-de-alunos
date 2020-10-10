99cda1c53d1552bf8d03898de7983c3a1f5e2845
Leonardo Bispo <leonardobispo1000@gmail.com>
2020-10-10 16:06:19 -0300

    adicionando arquivos

--- dist/app.js
+++ dist/app.js
@@ -1,21 +1,35 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
-var _path = require('path');
+Object.defineProperty(exports, '__esModule', { value: true }); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; } const _dotenv = require('dotenv');
+
+const _dotenv2 = _interopRequireDefault(_dotenv);
+const _path = require('path');
 
 _dotenv2.default.config(); // configurando o dotenv baseando no ".env" criado na raiz do projeto
 
 require('./database');
 
-var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+const _express = require('express');
+
+const _express2 = _interopRequireDefault(_express);
 // importação das rotas
-var _homeRoute = require('./routes/homeRoute'); var _homeRoute2 = _interopRequireDefault(_homeRoute);
-var _userRoute = require('./routes/userRoute'); var _userRoute2 = _interopRequireDefault(_userRoute);
-var _tokenRoute = require('./routes/tokenRoute'); var _tokenRoute2 = _interopRequireDefault(_tokenRoute);
-var _alunoRoute = require('./routes/alunoRoute'); var _alunoRoute2 = _interopRequireDefault(_alunoRoute);
-var _fotoRoute = require('./routes/fotoRoute'); var _fotoRoute2 = _interopRequireDefault(_fotoRoute);
+const _homeRoute = require('./routes/homeRoute');
+
+const _homeRoute2 = _interopRequireDefault(_homeRoute);
+const _userRoute = require('./routes/userRoute');
+
+const _userRoute2 = _interopRequireDefault(_userRoute);
+const _tokenRoute = require('./routes/tokenRoute');
+
+const _tokenRoute2 = _interopRequireDefault(_tokenRoute);
+const _alunoRoute = require('./routes/alunoRoute');
+
+const _alunoRoute2 = _interopRequireDefault(_alunoRoute);
+const _fotoRoute = require('./routes/fotoRoute');
+
+const _fotoRoute2 = _interopRequireDefault(_fotoRoute);
 // criando tudo que vai rodar no app
 class App {
   constructor() {
-    this.app = _express2.default.call(void 0, ); // todo o express será rodado dentro da constante app
+    this.app = _express2.default.call(void 0); // todo o express será rodado dentro da constante app
     this.middlewares();
     this.routes();
   }
@@ -37,4 +51,4 @@ class App {
   }
 }
 
-exports. default = new App().app;
+exports.default = new App().app;
