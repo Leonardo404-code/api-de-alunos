b56d1c226101a8e4735f86f768e0a54d27b2b9a3
Leonardo Bispo <leonardobispo1000@gmail.com>
2020-10-31 23:22:51 -0300

    adicionando arquivos

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
+
 var _path = require('path');
 
 _dotenv2.default.config(); // configurando o dotenv baseando no ".env" criado na raiz do projeto
@@ -6,13 +7,23 @@ _dotenv2.default.config(); // configurando o dotenv baseando no ".env" criado na
 require('./database');
 
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+
 var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
+
+var _expressdelay = require('express-delay'); var _expressdelay2 = _interopRequireDefault(_expressdelay);
+
 // importação das rotas
+
 var _homeRoute = require('./routes/homeRoute'); var _homeRoute2 = _interopRequireDefault(_homeRoute);
+
 var _userRoute = require('./routes/userRoute'); var _userRoute2 = _interopRequireDefault(_userRoute);
+
 var _tokenRoute = require('./routes/tokenRoute'); var _tokenRoute2 = _interopRequireDefault(_tokenRoute);
+
 var _alunoRoute = require('./routes/alunoRoute'); var _alunoRoute2 = _interopRequireDefault(_alunoRoute);
+
 var _fotoRoute = require('./routes/fotoRoute'); var _fotoRoute2 = _interopRequireDefault(_fotoRoute);
 
 const whiteList = [
@@ -28,21 +39,23 @@ const corsOptions = {
     }
   },
 };
+
 // criando tudo que vai rodar no app
 class App {
   constructor() {
     this.app = _express2.default.call(void 0, ); // todo o express será rodado dentro da constante app
-    this.middlewares();
-    this.routes();
+    this.middlewares(); // objeto middlewares
+    this.routes(); // objeto de rotas
   }
 
   // configurando os middlewares para funcioarem corretamente
   middlewares() {
     this.app.use(_cors2.default.call(void 0, corsOptions));
     this.app.use(_helmet2.default.call(void 0, ));
+    this.app.use(_expressdelay2.default.call(void 0, 2000)); // Delay proposital de resposta da api
     this.app.use(_express2.default.urlencoded({ extended: true }));
-    this.app.use(_express2.default.json());
-    this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
+    this.app.use(_express2.default.json()); // Definindo que a API vai apresentar informações em arquivo JSON
+    this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images'))); // Rota das imagens
   }
 
   // as rotas da api
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+
 import { resolve } from 'path';
 
 dotenv.config(); // configurando o dotenv baseando no ".env" criado na raiz do projeto
@@ -6,13 +7,23 @@ dotenv.config(); // configurando o dotenv baseando no ".env" criado na raiz do p
 import './database';
 
 import express from 'express';
+
 import cors from 'cors';
+
 import helmet from 'helmet';
+
+import delay from 'express-delay';
+
 // importação das rotas
+
 import homeRoute from './routes/homeRoute';
+
 import userRoutes from './routes/userRoute';
+
 import tokenRoutes from './routes/tokenRoute';
+
 import alunoRoutes from './routes/alunoRoute';
+
 import fotoRoutes from './routes/fotoRoute';
 
 const whiteList = [
@@ -28,21 +39,23 @@ const corsOptions = {
     }
   },
 };
+
 // criando tudo que vai rodar no app
 class App {
   constructor() {
     this.app = express(); // todo o express será rodado dentro da constante app
-    this.middlewares();
-    this.routes();
+    this.middlewares(); // objeto middlewares
+    this.routes(); // objeto de rotas
   }
 
   // configurando os middlewares para funcioarem corretamente
   middlewares() {
     this.app.use(cors(corsOptions));
     this.app.use(helmet());
+    this.app.use(delay(2000)); // Delay proposital de resposta da api
     this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(express.json());
-    this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
+    this.app.use(express.json()); // Definindo que a API vai apresentar informações em arquivo JSON
+    this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images'))); // Rota das imagens
   }
 
   // as rotas da api
